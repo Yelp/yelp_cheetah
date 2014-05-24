bcf4736e96491a841e88bbe24a49dd902d65ce7d
Anthony Sottile <asottile@umich.edu>
2014-05-24 12:56:17 -0700

    Remove safeConvert

--- Cheetah/DummyTransaction.py
+++ Cheetah/DummyTransaction.py
@@ -1,41 +1,15 @@
 """Provides Transactional buffering support for cheetah."""
 
-import logging
-
 
 class DummyResponse(object):
     def __init__(self):
         self._outputChunks = []
 
-    def safeConvert(self, chunk):
-        # Exceptionally gross, but the safest way
-        # I've found to ensure I get a legit unicode object
-        if not chunk:
-            return u''
-        if isinstance(chunk, unicode):
-            return chunk
-        try:
-            return chunk.decode('utf-8', 'strict')
-        except UnicodeDecodeError:
-            try:
-                return chunk.decode('latin-1', 'strict')
-            except UnicodeDecodeError:
-                return chunk.decode('ascii', 'ignore')
-        except AttributeError:
-            return unicode(chunk, errors='ignore')
-        return chunk
-
     def write(self, value):
         self._outputChunks.append(value)
 
-    def getvalue(self, outputChunks=None):
-        chunks = outputChunks or self._outputChunks
-        try:
-            return u''.join(chunks)
-        except UnicodeDecodeError:
-            logging.debug('Trying to work around a UnicodeDecodeError in getvalue()')
-            logging.debug('...perhaps you could fix "%s" while you\'re debugging')
-            return ''.join((self.safeConvert(c) for c in chunks))
+    def getvalue(self):
+        return u''.join(self._outputChunks)
 
 
 class DummyTransaction(object):
