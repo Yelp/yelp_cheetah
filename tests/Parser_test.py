60c74e22eef313fafad591950f80a62cc7b24b9e
Anthony Sottile <asottile@umich.edu>
2014-05-27 08:59:45 -0700

    Clean up Parser_test.

--- tests/Parser_test.py
+++ tests/Parser_test.py
@@ -1,42 +1,38 @@
-import unittest
+import pytest
 
-from Cheetah import Parser
+from Cheetah.Parser import ArgList
 
 
-class ArgListTest(unittest.TestCase):
-    def setUp(self):
-        super(ArgListTest, self).setUp()
-        self.al = Parser.ArgList()
+@pytest.yield_fixture
+def arglist():
+    yield ArgList()
 
-    def test_merge1(self):
-        '''
-            Testing the ArgList case results from Template.Preprocessors.test_complexUsage
-        '''
-        self.al.add_argument('arg')
-        expect = [('arg', None)]
 
-        self.assertEquals(expect, self.al.merge())
+def test_ArgList_merge1(arglist):
+    """Testing the ArgList case results from
+    Template.Preprocessors.test_complexUsage
+    """
+    arglist.add_argument('arg')
+    assert arglist.merge() == [('arg', None)]
 
-    def test_merge2(self):
-        '''
-            Testing the ArgList case results from SyntaxAndOutput.BlockDirective.test4
-        '''
-        self.al.add_argument('a')
-        self.al.add_default('999')
-        self.al.next()
-        self.al.add_argument('b')
-        self.al.add_default('444')
 
-        expect = [(u'a', u'999'), (u'b', u'444')]
+def test_ArgList_merge2(arglist):
+    """Testing the ArgList case results from
+    SyntaxAndOutput.BlockDirective.test4
+    """
+    arglist.add_argument('a')
+    arglist.add_default('999')
+    arglist.next()
+    arglist.add_argument('b')
+    arglist.add_default('444')
 
-        self.assertEquals(expect, self.al.merge())
+    assert arglist.merge() == [(u'a', u'999'), (u'b', u'444')]
 
-    def test_merge3(self):
-        '''
-            Testing the ArgList case results from SyntaxAndOutput.BlockDirective.test13
-        '''
-        self.al.add_argument('arg')
-        self.al.add_default("'This is my block'")
-        expect = [('arg', "'This is my block'")]
 
-        self.assertEquals(expect, self.al.merge())
+def test_merge3(arglist):
+    """Testing the ArgList case results
+    from SyntaxAndOutput.BlockDirective.test13
+    """
+    arglist.add_argument('arg')
+    arglist.add_default("'This is my block'")
+    assert arglist.merge() == [('arg', "'This is my block'")]
