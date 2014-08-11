8338b9e8ffa51e919eae5145604ff9092995c747
Anthony Sottile <asottile@umich.edu>
2014-08-11 11:30:32 -0700

    v0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, Extension
 
 setup(
     name="yelp_cheetah",
-    version='0.2.0a',
+    version='0.2.0',
     description='cheetah, hacked by yelpers',
     classifiers=[
         'Intended Audience :: Developers',
