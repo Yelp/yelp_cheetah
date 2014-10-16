1f2042033b621be47a86b69df6b2a1c3175106e5
Anthony Sottile <asottile@umich.edu>
2014-10-15 17:31:43 -0700

    Simplify settings manager.

--- Cheetah/SettingsManager.py
+++ Cheetah/SettingsManager.py
@@ -1,41 +1,5 @@
 from __future__ import unicode_literals
 
-import re
-from io import StringIO
-from tokenize import Number
-
-from Cheetah import five
-
-if five.PY2:  # pragma: no cover (PY2)
-    from ConfigParser import ConfigParser  # pylint:disable=import-error
-else:  # pragma: no cover (PY3)
-    from configparser import ConfigParser  # pylint:disable=import-error
-
-
-numberRE = re.compile(Number)
-
-
-def mergeNestedDictionaries(dict1, dict2):
-    """Recursively merge the values of dict2 into dict1.
-
-    This little function is very handy for selectively overriding settings in a
-    settings dictionary that has a nested structure.
-    """
-    for key, val in dict2.items():
-        if key in dict1 and isinstance(val, dict) and isinstance(dict1[key], dict):
-            dict1[key] = mergeNestedDictionaries(dict1[key], val)
-        else:
-            dict1[key] = val
-    return dict1
-
-
-class ConfigParserCaseSensitive(ConfigParser):
-    """A case sensitive version of the standard Python ConfigParser."""
-
-    def optionxform(self, optionstr):
-        """Don't change the case as is done in the default implemenation."""
-        return optionstr
-
 
 def convert_value(s):
     if s.lower() == 'none':
@@ -47,33 +11,8 @@ def convert_value(s):
     return s
 
 
-def readSettingsFromConfigFileObj(file_obj):
-    """Return the settings from a config file that uses the syntax accepted by
-    Python's standard ConfigParser module (like Windows .ini files).
-
-    NOTE: this method maintains case unlike the ConfigParser module
-
-    All setting values are initially parsed as strings.
-    However, the following value conversions are applied:
-
-    * The string 'True' will be converted to a Python truth value
-    * The string 'False' will be converted to a Python false value
-    """
-    parser = ConfigParserCaseSensitive()
-    parser.readfp(file_obj)
-    assert 'globals' in parser.sections()
-
-    return dict(
-        (k, convert_value(parser.get('globals', k)))
-        for k in parser.options('globals')
-    )
-
-
-class SettingsManager(object):  # pylint:disable=abstract-class-not-used
+class SettingsManager(object):
     """A mixin class that provides facilities for managing application settings.
-
-    SettingsManager is designed to work well with nested settings dictionaries
-    of any depth.
     """
 
     def __init__(self):
@@ -82,15 +21,7 @@ class SettingsManager(object):  # pylint:disable=abstract-class-not-used
         self._initializeSettings()
 
     def _initializeSettings(self):
-        """A hook that allows for complex setting initialization sequences that
-        involve references to 'self' or other settings.  For example:
-              self._settings['myCalcVal'] = self._settings['someVal'] * 15
-        This method should be called by the class' __init__() method when needed.
-        The dummy implementation should be reimplemented by subclasses.
-        """
         raise NotImplementedError
-
-    # core post startup methods
 
     def setting(self, name):
         """Get a setting from self._settings, with or without a default value."""
@@ -106,12 +37,13 @@ class SettingsManager(object):  # pylint:disable=abstract-class-not-used
 
     def updateSettings(self, newSettings):
         """Update the settings with a selective merge or a complete overwrite."""
-        mergeNestedDictionaries(self._settings, newSettings)
+        self._settings.update(newSettings)
 
-    def updateSettingsFromConfigStr(self, configStr):
-        """See the docstring for .updateSettingsFromConfigFile()
-        """
-        configStr = '[globals]\n' + configStr.strip()
-        inFile = StringIO(configStr)
-        newSettings = readSettingsFromConfigFileObj(inFile)
-        self.updateSettings(newSettings)
+    def updateSettingsFromConfigStr(self, config_str):
+        """See the docstring for updateSettingsFromConfigFile()"""
+        values = [line.split('=') for line in config_str.strip().splitlines()]
+        settings = dict(
+            (key.strip(), convert_value(value.strip()))
+            for key, value in values
+        )
+        self.updateSettings(settings)
--- tests/SettingsManager_test.py
+++ tests/SettingsManager_test.py
@@ -1,16 +1,6 @@
 import pytest
 
 from Cheetah.SettingsManager import convert_value
-from Cheetah.SettingsManager import mergeNestedDictionaries
-
-
-def test_mergeDictionaries():
-    left = {'foo': 'bar', 'abc': {'a': 1, 'b': 2, 'c': (3,)}}
-    right = {'xyz': (10, 9)}
-    expect = {'xyz': (10, 9), 'foo': 'bar', 'abc': {'a': 1, 'c': (3,), 'b': 2}}
-
-    result = mergeNestedDictionaries(left, right)
-    assert result == expect
 
 
 @pytest.mark.parametrize(
