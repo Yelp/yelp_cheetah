b952e8bbe03eeeb7de7624c2d6447cf3ef248a6b
Buck Golemon <buck@yelp.com>
2015-01-23 17:30:17 -0800

    back to dev mode

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="yelp_cheetah",
-    version='0.7.0',
+    version='0.8.0-dev',
     description='cheetah, hacked by yelpers',
     classifiers=[
         'Intended Audience :: Developers',
